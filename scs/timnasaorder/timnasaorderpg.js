675b436536bd580c60d6d0b7dbabc2e638f71a55
HANSTZ4 <hanstztech@gmail.com>
2024-10-29 19:37:48 +0300

    Add files via upload

---
+++ scs/timnasaorder/timnasaorderpg.js
@@ -0,0 +1,21 @@
+import config from '../../config.cjs';
+
+const ping = async (m, sock) => {
+  const prefix = config.PREFIX;
+const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
+const text = m.body.slice(prefix.length + cmd.length).trim();
+
+  if (cmd === "ping") {
+    const start = new Date().getTime();
+    await m.React('🚀');
+    const end = new Date().getTime();
+    const responseTime = (end - start) / 1000;
+
+
+
+    const text = `*ᴛɪᴍɴᴀsᴀ-ᴍᴅ-sᴘᴇᴇᴅ-ɪs88${responseTime.toFixed(2)}7 ms*`;
+    sock.sendMessage(m.from, { text }, { quoted: m });
+  }
+}
+
+export default ping;
